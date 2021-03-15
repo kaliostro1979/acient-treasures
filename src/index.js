3edd3fc6887a848346fe447b8ed4739020502222
Misha <abachyanm@gmail.com>
2021-03-15 12:52:09 +0400

    Redux dev tools part removed from index.js for correctly run application

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ import {Provider} from "react-redux";
 
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
 ))
 
 
