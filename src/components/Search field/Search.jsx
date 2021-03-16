ddd9486d3a3748d20f49f9c660827650e7dc2c5b
Misha <abachyanm@gmail.com>
2021-03-16 22:44:38 +0400

    Bug fix

--- src/components/Search field/Search.jsx
+++ src/components/Search field/Search.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from "react-redux";
 import {filterAllItems} from "../../redux/actions/getAllItems";
 
-const Search = (props)=>{
+const Search = ()=>{
     const search = useSelector(state=>state.searchField)
     const dispatch = useDispatch()
     const [input, setInput] = useState('')
@@ -16,11 +16,7 @@ const Search = (props)=>{
         dispatch(filterAllItems(input))
     },[input])
 
-    props.allItems.filter((i)=>{
-        return i.title.match(input)
-    })
 
-    
     return(
         <div className={ search ? "search-main search-main-open" : "search-main"}>
             <input type="text" placeholder="Enter product name" onChange={handleChange}/>
